506ef6f64afec3ab9a909661c617fa184a28041f
Joseph <tristian1992@126.com>
2020-06-15 18:13:29 +0800

    datamodel

--- test_DM.py
+++ test_DM.py
@@ -25,7 +25,7 @@ minmax = preprocessing.MinMaxScaler()
 data_minmax = minmax.fit_transform(data)
 
 #MaxAbs标准化
-#建立MinMaxScaler对象
+#建立MinMaxScaler对象等深分箱
 maxabs = preprocessing.MaxAbsScaler()
 # 标准化处理
 data_maxabs = maxabs.fit_transform(data)
@@ -39,12 +39,7 @@ data_rob = robust.fit_transform(data)
 # 可视化数据展示
 # 建立数据集列表
 data_list = [data, data_zs, data_minmax, data_maxabs, data_rob]
-# 创建颜色列表
-color_list = ['blue', 'red', 'green', 'black', 'pink']
-# 创建标题样式处理
-for i, dt in enumerate(data_list):
-    # 子网格
-    plt.subplot(2, 3, i+1)
+# 创建颜色列表等深分箱
     # 数据画散点图
     plt.scatter(dt[:, 0], dt[:, 1], c=color_list[i])
     # 设置标题
@@ -57,7 +52,7 @@ plt.show()
 
 """
  2/数据清洗：重复值处理
-"""
+"""等深分箱
 sample = pd.DataFrame({'id':[1,1,1,1,3,4,5],
                        'name':['Mark','Bob','Bob','Mark','Miki','Sully','Rose'],
                        'score':[80,99,99,87,77,77,np.nan],
@@ -95,5 +90,67 @@ sample_1.score.isnull()
 sample_1.score.isnull().apply(int)
 
 """
- 2/数据清洗: 噪声处理
+ 2/数据清洗: 噪声处理 (异常值，离群值)
+
+ *//噪声值的处理方法很多，对于单变量，常见的方法有盖帽法、分箱法；多变量的处理方法为聚类法。
 """
+"""
+盖帽法
+*//盖帽法本质就是将一个seires中 大于，小于制定分为数的数值替换为该分为数对应的数值。
+"""
+
+def cap(x,quantile=[0.01,0.99]):
+    """盖帽法处理异常值
+    Args：
+        x：pd.Series列，连续变量
+        quantile：指定盖帽法的上下分位数范围
+    """
+
+# 生成分位数
+    Q01,Q99=x.quantile(quantile).values.tolist()
+
+# 替换异常值为指定的分位数
+    if Q01 > x.min():
+        x = x.copy()
+        x.loc[x<Q01] = Q01
+
+    if Q99 < x.max():
+        x = x.copy()
+        x.loc[x>Q99] = Q99
+
+    return(x)
+
+sample_test = pd.DataFrame({'normal':np.random.randn(1000)})
+sample_test.hist(bins=50)
+
+sample_test['normal'] = cap(sample_test['normal'], quantile=[0.01,0.09])
+
+"""
+分箱法
+*//分箱法通过考察数据的“近邻”来光滑有序数据的值。有序值分布到一些桶或箱中。
+分箱法将异常数据包含在了箱子中，在进行建模的时候，不直接进行到模型中，因而可以达到处理异常值的目的。
+"""
+sample = pd.DataFrame({'normal':np.random.randn(10)})
+
+# 等分n箱
+pd.cut(sample.normal, 5)
+# 为分箱贴上标签
+pd.cut(sample.normal, bins=5, labels=['bad',2,3,4,'good'])
+
+"""
+等深分箱
+*//等深分箱中，各个箱的宽度可能不一，但频数是几乎相等的，所以可以采用数据的分位数来进行分箱。
+依旧以之前的sample数据为例，现进行等深度分2箱，首先找到2箱的分位数.
+"""
+sample.normal.quantile([0,0.5,1])
+pd.cut(sample.normal, bins=sample.normal.quantile([0,0.5,1]), include_lowest=True, labels=['bad','good'])
+
+
+"""
+多变量异常值处理-聚类法
+*//通过快速聚类法将数据对象分组成为多个簇，在同一个簇中的对象具有较高的相似度，而不同的簇之间的对象差别较大。
+聚类分析可以挖掘孤立点以发现噪声数据，因为噪声本身就是孤立点
+"""
+
+
+
